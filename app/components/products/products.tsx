d61fdb389c6d003cb008943ba3b05c4678c86533
ahsan693 <221387@students.au.edu.pk>
2026-08-16 02:38:30 +0500

    first product page added

--- app/components/products/products.tsx
+++ app/components/products/products.tsx
@@ -218,7 +218,7 @@ const allProducts = [
     cta: "View Product +",   
     href: "/ultimategloves",           
     swatches: ["#B91C1C", "#0D0D0D"],
-    image: "/Products/04 Pro MMA  Training Gloves.png",
+    image: "/Products/MMATrainingGloves.png",
   },
   {
     name: "Boxing Focus Mitts and Training Pads",
