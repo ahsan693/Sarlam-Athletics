4e20ae50d7eec193bc283692b45e1236b86c7ea9
ahsan693 <221387@students.au.edu.pk>
2026-08-16 02:36:30 +0500

    first product page added

--- app/components/ultimate-fightgloves/ultimate-fightgloves.tsx
+++ app/components/ultimate-fightgloves/ultimate-fightgloves.tsx
@@ -286,11 +286,11 @@ export default function UltimateMmaFightGlovesPage() {
           {/* Left: Product Image */}
           <div className="w-full lg:w-[616px] shrink-0">
             <div className="bg-[#F9F9F9] rounded-none p-4 md:p-2 aspect-[4/5] md:aspect-square flex items-center justify-center lg:sticky lg:top-24">
-              <img
-                src="https://images.unsplash.com/photo-1584464491033-06628f3a6b7b?w=800&h=800&fit=crop"
-                alt="Ultimate MMA Fight Gloves"
-                className="w-[100%] h-[100%] md:w-[600px] md:h-[600px] object-cover md:object-contain mix-blend-multiply"
-              />
+   <img
+  src="/Products/MMATrainingGloves.png"
+  alt="Ultimate MMA Fight Gloves"
+  className="w-[100%] h-[100%] md:w-[600px] md:h-[600px] object-cover md:object-contain mix-blend-multiply"
+/>
             </div>
           </div>
 
