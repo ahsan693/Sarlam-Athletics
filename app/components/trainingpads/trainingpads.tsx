61c7861d04afb455692f3cde88f8547f5ee92969
ahsan693 <221387@students.au.edu.pk>
2026-08-16 14:50:30 +0500

    first product page added

--- app/components/trainingpads/trainingpads.tsx
+++ app/components/trainingpads/trainingpads.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import Header from "@/components/Header";
+import { Header } from "../home/home";
 
 // ─── Image Placeholder ───
 const ImagePlaceholder = ({
