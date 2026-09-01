39db3b1fad7b79ae9e6247a7137d542cf95235b6
ahsan693 <221387@students.au.edu.pk>
2026-09-01 16:48:13 +0500

    updated

--- app/components/contactus/contactus.tsx
+++ app/components/contactus/contactus.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useRef } from "react";
+import { useState, useRef } from "react"; 
 import Head from "next/head";
 import Link from "next/link";
 import { motion, useScroll, useTransform } from "framer-motion";
